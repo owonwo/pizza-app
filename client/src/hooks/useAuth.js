f3af5265b1880d391684aa361260ce4d7a83119d
Joseph Owonvwon <Joseph.owonwo@gmail.com>
2020-10-18 20:30:27 +0100

    Feat: A user can login.

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,38 +1,64 @@
 import React from "react";
-import { log } from "@wigxel/utils";
+// import { log } from "@wigxel/utils";
+import useFetch from 'use-http';
+import { BASE_URL, AUTH_TOKEN_KEY } from '../libs/constants';
+
+
+const getToken = () => localStorage.getItem(AUTH_TOKEN_KEY);
+const hasToken = () => !!getToken();
 
 const useAuth = () => {
-  const [errors, setError] = React.useState({ signin: null });
-  const [signedIn, setSignedIn] = React.useState("LOADING");
+  const fetch = useFetch(BASE_URL + '/api', {
+  	interceptors: {
+  		request({ options }) {
+  			if (hasToken())
+  				options.headers.Authorization = `Bearer ${getToken()}`;
 
-  const loginUser = ({ email, password }) => {
-    Promise.resolve()
-      .then(() => console.log("Login Success"))
-      .catch((err) => setError({ ...errors, signin: err.message }));
+  			return options;
+  		}
+  	}
+  });
+  const [errors, setError] = React.useState({ login: null });
+
+  const loginUser = async ({ email, password }) => {
+  	// clear Errors
+  	setError({ ...errors, login: false });
+
+  	// try login
+    const payload = await fetch.post('/login', { email, password })
+
+    if (fetch.response.ok) {
+      localStorage.setItem(AUTH_TOKEN_KEY, payload.access_token);
+      setError({ ...errors, login: false })
+      return Promise.resolve('Login Successful');
+    }
+
+    if (payload.message)
+    	setError({ ...errors, login: payload.message });
+   	return Promise.reject(Error('Login Failed'));
   };
 
-  const getUser = async (id, closure) => {
-    return Promise.resolve({ name: "James" });
-  };
+  const getAuthUser = async () => {
+    const payload = await fetch.get('/me');
+    if (fetch.response.ok) {
+    	return payload;
+    }
 
-  const userIsAdmin = async (id) => {
-    const user = await getUser(id);
-    if (user.privilege !== "SUPER") throw Error("Unauthorized User");
-
-    return log(user, "Authenticated User");
+    return Promise.reject('User not found!');
   };
 
   const logoutUser = () => {
     console.log("logging out user");
+    localStorage.removeItem(AUTH_TOKEN_KEY);
     return Promise.resolve();
   };
 
   return {
+    getAuthUser,
     logoutUser,
-    getUser,
     loginUser,
+    hasToken,
     errors,
-    signedIn,
   };
 };
 
--- client/src/libs/constants.js
+++ client/src/libs/constants.js
@@ -1,1 +1,2 @@
 export const BASE_URL = 'http://19376e8c8961.ngrok.io';
+export const AUTH_TOKEN_KEY = 'auth_token';
