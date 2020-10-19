545f9f23798acecff81c7fc06044d72afcfb24ad
Joseph Owonvwon <Joseph.owonwo@gmail.com>
2020-10-19 01:06:23 +0100

    Changed BASE_URL to live version.

--- src/libs/constants.js
+++ src/libs/constants.js
@@ -1,3 +1,5 @@
-export const BASE_URL = 'http://19376e8c8961.ngrok.io';
+export const BASE_URL = 
+	"https://pizza-king-app.herokuapp.com";
+	// 'https://19376e8c8961.ngrok.io';
 export const AUTH_TOKEN_KEY = 'auth_token';
 export const AUTH_USER_KEY = 'auth_user';
